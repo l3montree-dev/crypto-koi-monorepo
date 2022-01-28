8e8669c7c1225a7e919fea33efb1aa8f79c3250e
timbastin <bastin.tim@gmail.com>
2022-01-28 22:32:10 +0100

    Readded config§

--- packages/crypto-koi-app/src/config.ts
+++ packages/crypto-koi-app/src/config.ts
@@ -1,11 +1,10 @@
 import Constants from "expo-constants";
 export const Config = {
     restApiBaseUrl:
-        //Constants?.manifest?.extra?.API_URL ??
-        "http://192.168.2.133:8080/",
+        Constants?.manifest?.extra?.API_URL ?? "http://192.168.2.133:8080/",
     graphqlBaseUrl:
-        // (Constants?.manifest?.extra?.API_URL ??
-        "http://192.168.2.133:8080" + "/query",
+        (Constants?.manifest?.extra?.API_URL ?? "http://192.168.2.133:8080") +
+        "/query",
 
     secondsBetweenFeeding: 10 * 60,
 };
