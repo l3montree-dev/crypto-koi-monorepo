62e9faa2363d7015cdf471589d11a583b7d629fe
timbastin <bastin.tim@gmail.com>
2022-03-18 10:55:21 +0100

    Changed onboarding koi

--- packages/crypto-koi-app/app.config.js
+++ packages/crypto-koi-app/app.config.js
@@ -1,7 +1,7 @@
 export default {
     extra: {
         API_URL: "https://dev.api.crypto-koi.io",
-        // API_URL: "http://192.168.2.133:8080",
+        //API_URL: "http://192.168.2.133:8080",
     },
     splash: {
         image: "./assets/splash.png",
