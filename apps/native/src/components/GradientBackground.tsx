5e7bd80832ec502f2c0712485ec3ff5684a122b9
sebastian <sebastian.kawelke@l3montree.com>
2022-04-23 22:38:24 +0200

    Fixed various small bugs

--- apps/native/src/components/GradientBackground.tsx
+++ apps/native/src/components/GradientBackground.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { View } from "react-native";
+import { Platform, View, StyleSheet, Image } from "react-native";
 import Svg, { Defs, LinearGradient, Rect, Stop } from "react-native-svg";
 import { CustomColors } from "../styles/colors";
 import { DimensionUtils } from "../utils/DimensionUtils";
@@ -8,52 +8,69 @@ interface Props {
     inSafeAreaView: boolean;
 }
 
+const styleSheet = StyleSheet.create({
+    image: {
+        width: DimensionUtils.deviceWidth,
+        height: DimensionUtils.deviceHeight,
+    },
+});
+
 const GradientBackground: FunctionComponent<Props> = (props) => {
     return (
         <View pointerEvents={"none"}>
-            <Svg
-                width={DimensionUtils.deviceWidth}
-                height={DimensionUtils.screenHeight}
-            >
-                <Defs>
-                    <LinearGradient
-                        id="Gradient"
-                        x1="0%"
-                        y1={props.inSafeAreaView ? "10%" : "100"}
-                        x2="200%"
-                        y2={props.inSafeAreaView ? "150%" : "200%"}
-                    >
-                        <Stop
-                            offset="0%"
-                            stopColor={
-                                props.inSafeAreaView ? "#101A33" : "white"
-                            }
-                            stopOpacity={props.inSafeAreaView ? 1 : 0.05}
-                        />
-                        <Stop
-                            offset="25%"
-                            stopColor={CustomColors.bgDark}
-                            stopOpacity="1"
-                        />
-                        <Stop
-                            offset="500%"
-                            stopColor={
-                                props.inSafeAreaView
-                                    ? "#331019"
-                                    : CustomColors.cherry
-                            }
-                            stopOpacity={props.inSafeAreaView ? "1" : "0.35"}
-                        />
-                    </LinearGradient>
-                </Defs>
-                <Rect
-                    x="0"
-                    y="0"
+            {Platform.OS === "android" ? (
+                <Svg
                     width={DimensionUtils.deviceWidth}
                     height={DimensionUtils.screenHeight}
-                    fill="url(#Gradient)"
+                >
+                    <Defs>
+                        <LinearGradient
+                            id="Gradient"
+                            x1="0%"
+                            y1={props.inSafeAreaView ? "10%" : "100"}
+                            x2="200%"
+                            y2={props.inSafeAreaView ? "150%" : "200%"}
+                        >
+                            <Stop
+                                offset="0%"
+                                stopColor={
+                                    props.inSafeAreaView ? "#101A33" : "white"
+                                }
+                                stopOpacity={props.inSafeAreaView ? 1 : 0.05}
+                            />
+                            <Stop
+                                offset="25%"
+                                stopColor={CustomColors.bgDark}
+                                stopOpacity="1"
+                            />
+                            <Stop
+                                offset="500%"
+                                stopColor={
+                                    props.inSafeAreaView
+                                        ? "#331019"
+                                        : CustomColors.cherry
+                                }
+                                stopOpacity={
+                                    props.inSafeAreaView ? "1" : "0.35"
+                                }
+                            />
+                        </LinearGradient>
+                    </Defs>
+                    <Rect
+                        x="0"
+                        y="0"
+                        width={DimensionUtils.deviceWidth}
+                        height={DimensionUtils.screenHeight}
+                        fill={"url(#Gradient)"}
+                    />
+                </Svg>
+            ) : (
+                <Image
+                    style={styleSheet.image}
+                    accessibilityIgnoresInvertColors
+                    source={require("../../assets/image/background_gradient.png")}
                 />
-            </Svg>
+            )}
         </View>
     );
 };
