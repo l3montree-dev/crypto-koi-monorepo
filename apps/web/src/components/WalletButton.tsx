f699071ed20547d4c917b0218b41f6024dd35453
timbastin <bastin.tim@gmail.com>
2022-04-29 19:30:17 +0200

    Added Wallet information to wallet button

--- apps/web/src/components/WalletButton.tsx
+++ apps/web/src/components/WalletButton.tsx
@@ -5,7 +5,8 @@ import Image from 'next/image'
 
 interface Props {
     onClick: () => void
-    wallet: { descriptor: WalletDescriptor } | null
+
+    wallet: { descriptor: WalletDescriptor; address: string } | null
 }
 const WalletButton: FunctionComponent<Props> = (props) => {
     const { wallet } = props
@@ -17,20 +18,25 @@ const WalletButton: FunctionComponent<Props> = (props) => {
         )
     }
     return (
-        <Button
-            leftIcon={
-                <Image
-                    width={30}
-                    height={30}
-                    alt={wallet.descriptor.name}
-                    src={wallet.descriptor.iconUrl}
-                />
-            }
-            isFullWidth
-            className="rounded-lg"
-        >
-            <span>Connected with {wallet.descriptor.name}</span>
-        </Button>
+        <>
+            <Button
+                leftIcon={
+                    <Image
+                        width={30}
+                        height={30}
+                        alt={wallet.descriptor.name}
+                        src={wallet.descriptor.iconUrl}
+                    />
+                }
+                isFullWidth
+                className="rounded-lg"
+            >
+                <span>Connected with {wallet.descriptor.name}</span>
+            </Button>
+            <small className="flex flex-row pt-2 justify-end text-right w-full">
+                {wallet.address}
+            </small>
+        </>
     )
 }
 
